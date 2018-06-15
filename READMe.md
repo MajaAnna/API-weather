e0649f5e55603b3fd32d853dd370a47a2613f02f
MajaAnna <majaprzyjazna@gmail.com>
2018-06-15 10:39:53 +0200

    error section

--- READMe.md
+++ READMe.md
@@ -1,5 +1,5 @@
 # API-weather application
-The application shows weather information got from Open Weather Map API.
+The application shows weather information from Open Weather Map API.
 The forecast can be found according to the city's name.
 
 Life preview is available [here](https://majaanna.github.io/nasa-images/)
--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const url = 'api.openweathermap.org/data/2.5/weather?q=',
       cityName = $('#city').val(),
       key = '48066df5beb56b726aa679490b0115e4';
 
-
-
-(()=>{
+      
+$(()=>{
     weatherLoading();
 })
 
@@ -16,6 +15,28 @@ let weatherLoading = () => {
         weatherInformation(resp)
     }).fail(function(error){
         console.log(error.status)
+        errorLoading(error.status)
     })
+}
+
+let weatherInformation = (resp) => {
+    const errorMessage = $('.error-message'),
+          searchingBtn = $('#search');
+
+          console.log(searchingBtn)
+
+    searchingBtn.on('click', function(){
+        console.log('click')
+        if (cityName == ''){
+            errorMessage.text('The field cannot be empty')
+        } else {
+    
+        }
+    })
+}
+
+let errorLoading = status => {
+    const errorLoading = $('.error-loading');
+    errorLoading.text("There is an error number: " + status);
 }
 
